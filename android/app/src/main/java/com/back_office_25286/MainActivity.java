70ba95f9f7fe1e4e4147b07ac003755e9f9c8347
Crowdbotics <team@crowdbotics.com>
2021-03-26 16:08:33 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/back_office_25286/MainActivity.java
@@ -0,0 +1,15 @@
+package com.back_office_25286;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "back_office_25286";
+    }
+}
---
+++ src/config/app.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://back-office-25286.botics.co",
+  defaultTimeout: 5000
+};
